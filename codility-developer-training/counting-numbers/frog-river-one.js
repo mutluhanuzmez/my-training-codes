8865ece4dc2060fcbc9a9edc86a3ff9ed618633d
Mutluhan Üzmez <mutluhanuzmez@gmail.com>
2022-01-25 23:01:33 +0300

    codility(counting-numbers): frog river one

---
+++ codility-developer-training/counting-numbers/frog-river-one.js
@@ -0,0 +1,22 @@
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
+
+function solution(X, A) {
+    // write your code in JavaScript (Node.js 8.9.4)
+    let leaveMap = new Map();
+
+    for (let i = 1; i <= X; i++) {
+        leaveMap.set(i, false);
+    }
+
+    for (let i = 0; i < A.length; i++) {
+        if (leaveMap.has(A[i])) {
+            leaveMap.delete(A[i]);
+        }
+        if (leaveMap.size == 0) {
+            return i;
+        }
+    }
+
+    return -1;
+}
