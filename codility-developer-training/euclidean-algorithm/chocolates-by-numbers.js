a33765c9e74dc884e7416553fadc55f8103aa2ec
Mutluhan Üzmez <mutluhanuzmez@gmail.com>
2022-02-17 00:02:42 +0300

    codility(euclidian):optimal gcd solution

--- codility-developer-training/euclidean-algorithm/chocolates-by-numbers.js
+++ codility-developer-training/euclidean-algorithm/chocolates-by-numbers.js
@@ -1,25 +1,31 @@
 // you can write to stdout for debugging purposes, e.g.
 // console.log('this is a debug message');
 
-function solution(N, M) {
-    // write your code in JavaScript (Node.js 8.9.4)
+// There are N chocolates in a circle. Count the number of chocolates you will eat.
 
-    let counter = 0;
-    let wrappersSet = new Set();
-    let startPoint = 0;
-    while(true){
-        let mod = startPoint % N;
-        if(wrappersSet.has(mod)){
-            return counter;
-        }
-        
-        if(mod == startPoint){
-            wrappersSet.add(mod);
-            counter++;
-            startPoint += M;
-            startPoint = startPoint % N;
-        }
+const floorDiv = (a, b) => {
+    return Math.floor(a / b);
+}
+
+const gcd = (a, b, res) => {
+    if (a == b) {
+        return res * a;
+    } else if ((a % 2 == 0) && (b % 2 == 0)) {
+        return gcd(floorDiv(a, 2), floorDiv(b, 2), 2*res);
+    } else if (a % 2 == 0) {
+        return gcd(floorDiv(a, 2), b, res);
+    } else if (b % 2 == 0) {
+        return gcd(a, floorDiv(b, 2), res);
+    } else if (a > b) {
+        return gcd(a - b, b, res);
+    } else {
+        return gcd(a, b - a, res);
     }
 }
 
-console.log(solution(10,7));
+function solution(N, M) {
+    // write your code in JavaScript (Node.js 8.9.4)
+    return N/gcd(N,M,1);
+}
+
+console.log(solution(10, 1));
