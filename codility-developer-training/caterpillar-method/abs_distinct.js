26fb4c08cd9e2cc7cf3d05a5120813a6101efb1d
Mutluhan Üzmez <mutluhanuzmez@gmail.com>
2022-02-18 23:08:11 +0300

    codility(caterpillar): template

---
+++ codility-developer-training/caterpillar-method/abs_distinct.js
@@ -0,0 +1,8 @@
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
+
+//template code
+
+function solution(A) {
+    // write your code in JavaScript (Node.js 8.9.4)
+}
